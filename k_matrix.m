ec073e0357b9c172438f8706ea433568b6a0b02a
Manish D'Silva <45946138+manishdsilva@users.noreply.github.com>
2020-02-08 18:22:52 +0530

    Update k_matrix.m

--- k_matrix.m
+++ k_matrix.m
@@ -1,18 +1,68 @@
-pkg load control
-A = [   0.            1.            0.            0.     ;   
-    0.          -3.52645454  -16.65793859   0.22921955;
-    0.            0.            0.            1.        ;
-    0.         -26.94257673 162.13373685   1.75126749];
-B = [ 0.        ;
-  0.51118094;
-  0.        ;
- 3.90548961;];
+####%robot parameters
+##mb = 0.987; %mass of robot
+##mw = 0.025; %mass of wheels
+##jb = 0.00383; %moment of inertia about the centre of mass
+##r = 0.04; %radius of wheels
+##jw = 4E-05; %moment of inertia for the wheels
+##l = 0.102; %distance from wheel axle to CoM
+##ke = 0.855;
+##km = 0.316;
+##R = 7.2; %motor resistance
+##g = 9.81; %gravity
+##b = 0.002; %Viscous friction constant
 
-Q = [1 0 0 0;
- 0 0 0 0;
- 0 0 1 0;
- 0 0 0 0;];
- R = 1;
- 
-[K,S,P] = lqr(A,B,Q,R);
-K
+pkg load control;
+pkg load signal
+mb = (1.0-0.310);               #Robot Mass Kg 
+mw = (91+310)/1000;         #Wheel Mass Kg
+h = 0.245       ;         #height m
+w = 0.07        ;        #width m #not distance between wheels
+jb = (mb*(h*h + w*w))/12 #Moment of Inertia about center Kgm^2
+r = 0.065/2              ; #Radius m
+jw = 0.5*(mw)*r*r     ;#Moment of Inertia of wheels Kgm^2
+l = (0.07)              ; ### Distance of wheel to center of mass
+ke = 0.3183             ;### EMF Constant Vs/rad
+km = 0.41675       ;## Torque Constant Nm/A
+R = 4               ;### Motor Resistance ohm
+g = 9.81               ; #m/s^2     
+b = 0.002               ;### Viscous Friction constant Nms/rad
+
+alp = (2*(R*b - ke*km)*(mb*l*l + mb*r*l +jb))/ R*(2*(jb*jw + jw*l*l*mb +
+jb*mw*r*r + l*l*mb*mw*r*r)+jb*mb*r*r);
+bet = (-l*l*mb*mb*g*r*r)/(jb*(2*jw + mb*r*r + 2*mw*r*r) + 2*jw*l*l*mb +
+2*l*l*mb*mw*r*r);
+gam = (-2*(R*b -ke*km)*(2*jw + mb*r*r + 2*mw*r*r + l*mb*r))/(R*r*(2*(jb*jw
++ jw*l*l*mb + jb*mw*r*r + l*l*mb*mw*r*r)+jb*mb*r*r));
+delt = (l*mb*g*(2*jw + mb*r*r + 2*mw*r*r))/(2*jb*jw + 2*jw*l*l*mb +
+jb*mb*r*r +2*jb*mw*r*r + 2*l*l*mb*mw*r*r);
+chi = (km*r)/(R*b - ke*km);
+A = [ 0 1 0 0;
+      0 alp bet -r*alp;
+      0 0 0 1;
+      0 gam delt -r*alp];
+      
+B = [0; alp*chi; 0; gam*chi];
+
+C = [ 1 0 0 0;
+      0 1 0 0;
+      0 0 1 0;
+      0 0 0 1];
+      
+D = [0;0;0;0];
+
+Q=C'*C;
+T = 10;
+Q =   [10 0 0 0 ; 
+       0 5 0 0 ; 
+       0 0 100 0 ; 
+       0 0 0 5];
+##[n,d]=ss2tf(A,B,C,D);
+##n  %num
+##d  %den
+sys = ss(A,B,C,D);
+sys = c2d(sys,0.01)
+R = 100;
+#[K,S,e] = dlqr(sys.a,sys.b,Q,R);
+[K,S,e] = lqr(A,B,Q,R);
+e
+strcat("float k1 = ",num2str(K(1)),",k2 = ",num2str(K(2)),",k3 = ",num2str(K(3)),",k4 = ",num2str(K(4)),";")
